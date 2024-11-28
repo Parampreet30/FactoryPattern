ea82d38e4a90edc24d0aaf8fafc0e5869dd9e4d7
Parampreet30 <A00264414@mycambrian.ca>
2024-11-28 01:22:33 +0000

    program.cs updated

--- FactoryPattern/FactoryMethod/StorageFactory.cs
+++ FactoryPattern/FactoryMethod/StorageFactory.cs
@@ -1,16 +1,25 @@
 namespace FactoryPattern.FactoryMethod;
 using FactoryPattern.Models;
 
+public enum StorageType
+{
+    File = 1,
+    Database = 2,
+}
+
 public class StorageFactory
 {
- public IStorage GetStorage(string storageType)
+ //public IStorage GetStorage(string storageType)
+ public IStorage GetStorage(StorageType storageType)
  {
     switch(storageType)
     {
-        case"file":
+        //case"file":
+        case StorageType.File:
         return new FileStorage();
         //break;
-        case "database":
+        //case "database":
+        case StorageType.Database:
         return new DataStorage();
         //break;
         default:
--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,14 +4,25 @@ StorageFactory factory = new StorageFactory();
 
 
 Console.WriteLine("What type of storage do you want?");
-var storageType = Console.ReadLine();
 
-if (storageType is not null)
-{
-    var storage = factory.GetStorage(storageType ?? "");
+Console.WriteLine("\t1. File");
+
+Console.WriteLine("\t1. Datbase");
+var storageString = Console.ReadLine();
+var storageType = int.Parse(storageString ?? "0");
+
+
+
+
+
+//if (storageType is not null)
+//{
+    //var storage = factory.GetStorage(storageType ?? "");
+    var storage = factory.GetStorage((StorageType)storageType);
+
 
 Console.WriteLine("What  is your data?");
   var data = Console.ReadLine();
 
   storage.SaveData(data ?? "");
-}
+//}
