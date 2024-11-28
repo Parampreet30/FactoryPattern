f938998331618d7f414ab2283ce9b63260b0e46c
Parampreet30 <A00264414@mycambrian.ca>
2024-11-28 01:03:42 +0000

    storageFactory.cv updated

--- FactoryPattern/FactoryMethod/StorageFactory.cs
+++ FactoryPattern/FactoryMethod/StorageFactory.cs
@@ -3,5 +3,19 @@ using FactoryPattern.Models;
 
 public class StorageFactory
 {
-
+ public IStorage GetStorage(string storageType)
+ {
+    switch(storageType)
+    {
+        case"file":
+        return new FileStorage();
+        //break;
+        case "database":
+        return new DataStorage();
+        //break;
+        default:
+        return new FileStorage();
+        //break;
+    }
+ }
 }
